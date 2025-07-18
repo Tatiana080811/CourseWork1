0e8b6533711f109a0bc2a407e738cb781c1b5c17
Tatiana080811 <Kazantseva2pochta@gmail.com>
2025-07-18 21:43:03 +0700

    CourseWork1

--- src/Employee.java
+++ src/Employee.java
@@ -1,78 +1,65 @@
 import java.util.Objects;
-
 public class Employee {
+    private static int nextId = 1;
 
-    private String name;
+    private final int id;
     private String firstName;
-    private String sureName;
+    private String lastName;
     private int department;
     private double salary;
-    private int id;
 
-    private static int counter = 1;
-    public String Employee(String name, String firstName, String sureName, int department, double salary) {
-        this.name = name;
+    public Employee(String firstName, String lastName, int department, double salary) {
+        this.id = nextId++;
         this.firstName = firstName;
-        this.sureName = sureName;
-        if (department < 1 || department > 5)
-            throw new IllegalArgumentException("Отдел должен быть числом от 1 до 5");
-        this.department = department;
-        this.salary = salary;
-        this.id = counter++;
-        return name;
+        this.lastName = lastName;
+        setDepartment(department);
+        setSalary(salary);
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getSureName() {
-        return sureName;
-    }
-    public double getSalary() {
-        return salary;
-    }
-    public int getDepartment() {
-        return department;
-    }
-    public int getId() {
-        return id;
+    public int getId() { return id; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public int getDepartment() { return department; }
+    public double getSalary() { return salary; }
+
+    public void setDepartment(int department) {
+        if (department >= 1 && department <= 5) {
+            this.department = department;
+        } else {
+            throw new IllegalArgumentException("Отдел должен быть между 1 и 5");
+        }
     }
 
-    public void setSalary (int department) {
-        if (department < 1 || department > 5)
-            throw new IllegalArgumentException("Отдел должен быть числом от 1 до 5");
-        this.department = department;
-    }
     public void setSalary(double salary) {
-        if (salary <= 0)
-            throw new IllegalArgumentException("Зарплата должна быть положительным числом");
-        this.salary = salary;
+        if (salary > 0) {
+            this.salary = salary;
+        } else {
+            throw new IllegalArgumentException("Зарплата должна быть положительной величиной");
+        }
     }
 
+    @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (!(obj instanceof Employee)) return false;
         Employee other = (Employee) obj;
-        return Objects.equals(firstName, other.firstName) &&
-                Objects.equals(name, other.name) &&
-                Objects.equals(sureName, other.sureName) &&
+        return id == other.id &&
+                Objects.equals(firstName, other.firstName) &&
+                Objects.equals(lastName, other.lastName) &&
                 department == other.department &&
-                Double.compare(salary, other.salary) == 0;
-    }
-    public int hashCode() {
-        return Objects.hash(firstName, name, sureName, department, salary);
+                Double.compare(other.salary, salary) == 0;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, department, salary);
+    }
+
+    @Override
     public String toString() {
-        return "ID: " + id + ", ФИО: " + firstName + ' ' + name + sureName + ", отдел: " + department +
-                ", зарплата: " + salary;
+        return "ID: " + id +
+                ", ФИО: " + firstName + ' ' + lastName +
+                ", Отдел: " + department +
+                ", Зарплата: " + salary;
     }
 }
-
-
-
-
-
--- src/Main.java
+++ src/Main.java
@@ -2,87 +2,77 @@ import java.util.Arrays;
 
 public class Main {
     private static Employee[] employees = new Employee[10];
-    public static void main(String[] args) {
-        employees[0] = new Employee("Иван", "Иванов", "Иванович", 1, 50_000);
-        employees[1] = new Employee("Мария", "Петрова", "Игоревна", 2, 70_000);
-        employees[2] = new Employee("Сергей", "Сергеев", "Сергеевич", 3, 90_000);
-        employees[3] = new Employee("Анна", "Смирнова", "Анатольевна", 4, 60_000);
-        employees[4] = new Employee("Алексей", "Васильев", "Васильевич", 5, 100_000);
+    public static void printAllEmployees() {
+        for (Employee emp : employees) {
+            if (emp != null) System.out.println(emp.toString());
+        }
+    }
+    public static double calculateTotalSalaries() {
+        double total = 0;
+        for (Employee emp : employees) {
+            if (emp != null) total += emp.getSalary();
+        }
+        return total;
+    }
 
+
+    public static Employee findMinSalaryEmployee() {
+        Employee minEmp = null;
+        for (Employee emp : employees) {
+            if (emp != null && (minEmp == null || emp.getSalary() < minEmp.getSalary())) {
+                minEmp = emp;
+            }
+        }
+        return minEmp;
+    }
+    public static Employee findMaxSalaryEmployee() {
+        Employee maxEmp = null;
+        for (Employee emp : employees) {
+            if (emp != null && (maxEmp == null || emp.getSalary() > maxEmp.getSalary())) {
+                maxEmp = emp;
+            }
+        }
+        return maxEmp;
+    }
+    public static double averageSalary() {
+        double sum = calculateTotalSalaries(); // Используем ранее созданный метод
+        long count = Arrays.stream(employees).filter(e -> e != null).count();
+
+        return count > 0 ? sum / count : 0;
+    }
+    public static void printFullNames() {
+        for (Employee emp : employees) {
+            if (emp != null) System.out.println(emp.getFirstName() + " " + emp.getLastName());
+        }
+    }
+
+    public static void main(String[] args) {
+        employees[0] = new Employee("Иван", "Иванов", 1, 80_000);
+        employees[1] = new Employee("Сергей", "Сергеев", 2, 90_000);
+        employees[2] = new Employee("Алексей", "Алексеев", 3, 100_000);
+        employees[3] = new Employee("Дмитрий", "Дмитриев", 4, 70_000);
+        employees[4] = new Employee("Михаил", "Михайлов", 5, 110_000);
+
+        System.out.println("\nСписок всех сотрудников:");
         printAllEmployees();
-        System.out.println("Сумма зарплат: " + calculateTotalSalary());
-        findMinSalaryEmployee();
-        findMaxSalaryEmployee();
-        System.out.println("Средняя зарплата: " + averageSalary());
+
+        System.out.println("\nСумма расходов на зарплаты: " + calculateTotalSalaries());
+
+        Employee minSalaryEmp = findMinSalaryEmployee();
+        if (minSalaryEmp != null) {
+            System.out.println("\nМинимальная зарплата у сотрудника: " + minSalaryEmp.getFirstName() + " " + minSalaryEmp.getLastName());
+        }
+
+        Employee maxSalaryEmp = findMaxSalaryEmployee();
+        if (maxSalaryEmp != null) {
+            System.out.println("\nМаксимальная зарплата у сотрудника: " + maxSalaryEmp.getFirstName() + " " + maxSalaryEmp.getLastName());
+        }
+
+        System.out.println("\nСредняя зарплата: " + averageSalary());
+
+        System.out.println("\nФИО всех сотрудников:");
         printFullNames();
     }
-
-    public static void printAllEmployees() {
-        for (Employee employee : employees) {
-            if (employee != null) {
-                System.out.println(employee.toString());
-            }
-        }
-    }
-
-    public static double calculateTotalSalary() {
-        double totalSalary = 0;
-        for (Employee employee : employees) {
-            if (employee != null) {
-                totalSalary += employee.getSalary();
-            }
-        }
-        return totalSalary;
-    }
-
-    public static void findMinSalaryEmployee() {
-        Employee minSalaryEmployee = null;
-        double minSalary = Double.MAX_VALUE;
-        for (Employee employee : employees) {
-            if (employee != null && employee.getSalary() < minSalary) {
-                minSalary = employee.getSalary();
-                minSalaryEmployee = employee;
-            }
-        }
-        if (minSalaryEmployee != null) {
-            System.out.println("Сотрудник с минимальной зарплатой: " + minSalaryEmployee.toString());
-        } else {
-            System.out.println("Сотрудники отсутствуют.");
-        }
-    }
-
-    public static void findMaxSalaryEmployee() {
-        Employee maxSalaryEmployee = null;
-        double maxSalary = Double.MIN_VALUE;
-        for (Employee employee : employees) {
-            if (employee != null && employee.getSalary() > maxSalary) {
-                maxSalary = employee.getSalary();
-                maxSalaryEmployee = employee;
-            }
-        }
-        if (maxSalaryEmployee != null) {
-            System.out.println("Сотрудник с максимальной зарплатой: " + maxSalaryEmployee.toString());
-        } else {
-            System.out.println("Сотрудники отсутствуют.");
-        }
-    }
-
-    public static double averageSalary() {
-        double sumOfSalaries = calculateTotalSalary();
-        int numberOfEmployees = 0;
-        for (Employee employee : employees) {
-            if (employee != null) {
-                numberOfEmployees++;
-            }
-        }
-        return numberOfEmployees > 0 ? sumOfSalaries / numberOfEmployees : 0;
-    }
-
-    public static void printFullNames() {
-        for (Employee employee : employees) {
-            if (employee != null) {
-                System.out.println(employee.getFirstName() + " " + employee.getName() + " " + employee.getFirstName());
-            }
-        }
-    }
 }
+
+
